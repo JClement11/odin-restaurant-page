9cfefeca4b5dab84b6f71cee2f64df51647b57c4
JClement11 <jclement0829@gmail.com>
2025-01-04 13:48:31 -0600

    Deployment commit

--- main.js
+++ main.js
@@ -7,26 +7,110 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/about.js":
+/*!**********************!*\
+  !*** ./src/about.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   aboutPage: () => (/* binding */ aboutPage)\n/* harmony export */ });\nfunction aboutPage() {\n    let content = document.querySelector(\"#content\");\n\n    const heading = document.createElement(\"h1\");\n    heading.textContent = \"Welcome to Big Boi Restaurant!\";\n    content.appendChild(heading);\n\n    const paragraph = document.createElement(\"p\");\n    paragraph.textContent = \"We are a restaurant made by big bois, for big bois.\";\n    content.appendChild(paragraph);\n}\n\n\n\n//# sourceURL=webpack://odin-restaurant-page/./src/about.js?");
+
+/***/ }),
+
+/***/ "./src/home.js":
+/*!*********************!*\
+  !*** ./src/home.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   homepage: () => (/* binding */ homepage)\n/* harmony export */ });\nfunction homepage() {\n    let content = document.querySelector(\"#content\");\n\n    const heading = document.createElement(\"h1\");\n    heading.textContent = \"Big Boi Restaurant\";\n    content.appendChild(heading);\n\n    const picture = document.createElement(\"img\");\n    picture.src = \"https://images.pexels.com/photos/12485977/pexels-photo-12485977.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1\";\n    content.appendChild(picture);\n\n    const paragraph = document.createElement(\"p\");\n    paragraph.textContent = \"This is the best restaurant for the biggest big bois!\";\n    content.appendChild(paragraph);\n}\n\n\n\n//# sourceURL=webpack://odin-restaurant-page/./src/home.js?");
+
+/***/ }),
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("console.log(\"hello\");\n\n//# sourceURL=webpack://odin-restaurant-page/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _home_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home.js */ \"./src/home.js\");\n/* harmony import */ var _about_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./about.js */ \"./src/about.js\");\n/* harmony import */ var _menu_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./menu.js */ \"./src/menu.js\");\n\n\n\n\n(0,_home_js__WEBPACK_IMPORTED_MODULE_0__.homepage)();\n\nlet content = document.querySelector(\"#content\");\nconst homeButton = document.querySelector(\"#home\");\nconst aboutButton = document.querySelector(\"#about\");\nconst menuButton = document.querySelector(\"#menu\");\n\nhomeButton.addEventListener(\"click\", () => {\n    content.textContent = \"\";\n    (0,_home_js__WEBPACK_IMPORTED_MODULE_0__.homepage)();\n});\n\naboutButton.addEventListener(\"click\", () => {\n    content.textContent = \"\";\n    (0,_about_js__WEBPACK_IMPORTED_MODULE_1__.aboutPage)();\n});\n\nmenuButton.addEventListener(\"click\", () => {\n    content.textContent = \"\";\n    (0,_menu_js__WEBPACK_IMPORTED_MODULE_2__.menuPage)();\n});\n\n//# sourceURL=webpack://odin-restaurant-page/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/menu.js":
+/*!*********************!*\
+  !*** ./src/menu.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   menuPage: () => (/* binding */ menuPage)\n/* harmony export */ });\nfunction menuPage() {\n    let content = document.querySelector(\"#content\");\n\n    const heading = document.createElement(\"h1\");\n    heading.textContent = \"Menu\";\n    content.appendChild(heading);\n\n    const paragraph = document.createElement(\"p\");\n    paragraph.textContent = \"Here is our Big Boi menu\";\n    content.appendChild(paragraph);\n}\n\n\n\n//# sourceURL=webpack://odin-restaurant-page/./src/menu.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
